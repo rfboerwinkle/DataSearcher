283af4645cb562bf14f83ec7da93999c22dcbab5
rfboerwinkle <rfboerwinkle@gmail.com>
2022-06-13 17:04:25 -0500

    Database manipulation using names, might use IDs later

--- META.py
+++ META.py
@@ -6,12 +6,12 @@ TRUE = "t"
 FALSE = "f"
 NA = "na"
 
-class DB:
-	def normalize(datum):
-		return datum.strip(" \n\t\r").lower()
+def normalize(datum):
+	return datum.strip(" \n\t\r").lower()
 
+class DB:
 	def __init__(self, name):
-		toCreate = not exists(name):
+		toCreate = not exists(name)
 
 		self.connection = sqlite3.connect(name)
 		self.cursor = self.connection.cursor()
@@ -19,21 +19,110 @@ class DB:
 		if toCreate:
 			print("Database doesn't exist, initializing one...")
 			# I didn't use ROWID becuase "VACUUM" reorganizes it
+			self.cursor.execute("""CREATE TABLE tagGroups (
+				id INTEGER PRIMARY KEY,
+				name TEXT NOT NULL UNIQUE
+			) WITHOUT ROWID""")
+			self.curTagGroupID = -1
+
 			self.cursor.execute("""CREATE TABLE tags (
-				id INTIGER PRIMARY KEY
-				groupName TEXT NOT NULL,
-				value TEXT
-			)""")
-			self.cursor.execute("INSERT INTO tags (groupName, value) VALUES (?,?,?)", (0, "exists", TRUE))
+				id INTEGER PRIMARY KEY,
+				tagGroupID INTEGER NOT NULL,
+				name TEXT NOT NULL,
+				FOREIGN KEY(tagGroupID) REFERENCES tagGroups(id)
+			) WITHOUT ROWID""")
+			self.curTagID = -1
 
 			self.cursor.execute("""CREATE TABLE data (
-				name TEXT NOT NULL,
-				tagID INTEGER NOT NULL
+				id INTEGER PRIMARY KEY,
+				name TEXT NOT NULL UNIQUE
+			) WITHOUT ROWID""")
+			self.curDatumID = -1
+
+			self.cursor.execute("""CREATE TABLE tagging (
+				datumID INTEGER NOT NULL,
+				tagID INTEGER NOT NULL,
+				FOREIGN KEY(datumID) REFERENCES data(id),
+				FOREIGN KEY(tagID) REFERENCES tags(id)
 			)""")
 
-	def addTagGroup(name, values=(None,)):
+			self.connection.commit()
+
+		else:
+			self.cursor.execute("SELECT MAX(id) FROM tagGroups")
+			self.curTagGroupID = self.cursor.fetchall()[0][0]
+			if self.curTagGroupID == None:
+				self.curTagGroupID = -1
+			print("Highest tagGroupID:", self.curTagGroupID)
+
+			self.cursor.execute("SELECT MAX(id) FROM tags")
+			self.curTagID = self.cursor.fetchall()[0][0]
+			if self.curTagID == None:
+				self.curTagID = -1
+			print("Highest tagID:", self.curTagID)
+
+			self.cursor.execute("SELECT MAX(id) FROM data")
+			self.curDatumID = self.cursor.fetchall()[0][0]
+			if self.curDatumID == None:
+				self.curDatumID = -1
+			print("Highest DatumID:", self.curDatumID)
+
+	def commit(self):
+		self.connection.commit()
+
+	def addTagGroup(self, name):
+		self.curTagGroupID += 1
+		self.cursor.execute("INSERT INTO tagGroups (id,name) VALUES (?,?)", (self.curTagGroupID, name))
+		return self.curTagGroupID
+
+	def addTag(self, tagGroupName, name): # TODO fix
+		self.curTagID += 1
+		self.cursor.execute("""INSERT INTO tags (id,tagGroupID,name) VALUES (?,
+			(SELECT id FROM tagGroups WHERE name = ? LIMIT 1)
+		,?)""", (self.curTagID, tagGroupName, name))
+		return self.curTagID
+
+	def addDatum(self, name):
+		self.curDatumID += 1
+		self.cursor.execute("INSERT INTO data (id,name) VALUES (?,?)", (self.curDatumID, name))
+		return self.curDatumID
+
+	def tagDatum(self, datumName, tagGroupName, tagName): # TODO fix
+		self.cursor.execute("""INSERT INTO tagging (datumID,tagID) VALUES (
+			(SELECT id FROM data WHERE name = ? LIMIT 1),
+			(SELECT id FROM tags WHERE name = ? AND tagGroupID =
+				(SELECT id FROM tagGroups WHERE name = ? LIMIT 1)
+			LIMIT 1)
+		)""", (datumName, tagName, tagGroupName))
+
+	def untagDatum(self, datumName, tagName):
 		pass
-	def addTagValue(groupName, value):
+
+	def getTagGroups(self):
+		self.cursor.execute("SELECT name FROM tagGroups ORDER BY name")
+		return self.cursor.fetchall()
+
+	def getTags(self, tagGroupName):
+		self.cursor.execute("""SELECT name FROM tags WHERE tagGroupID =
+			(SELECT id FROM tagGroups WHERE name = ? LIMIT 1)
+		ORDER BY name""", (tagGroupName,))
+		return self.cursor.fetchall()
+
+	def getData(self):
+		self.cursor.execute("SELECT name FROM data ORDER BY name")
+		return self.cursor.fetchall()
+
+	def getDatum(self, name):
+		self.cursor.execute("""SELECT tagGroups.name,tags.name FROM tags
+		JOIN tagGroups ON tags.tagGroupID = tagGroups.id
+		WHERE tags.id IN
+			(SELECT tagID FROM tagging WHERE datumID =
+				(SELECT id FROM data WHERE name = ? LIMIT 1)
+			)
+		ORDER BY tagGroups.name,tags.name""", (name,))
+		return self.cursor.fetchall()
+
+	def editTag(self, groupName, oldName, newName): # all data with this tag will change
 		pass
-	def addDatum(name, tagIDs=(0,)):
+	def editTagGroup(self, oldGroupName, newGroupName):
 		pass
--- runTime.py
+++ runTime.py
@@ -1,3 +1,30 @@
 from META import *
 
 trialDB = DB("data/trial.db")
+#trialDB.addTagGroup("leaf angle")
+#trialDB.addTag("leaf angle", "obtuse leaf")
+#trialDB.addTag("leaf angle", "acute leaf")
+#trialDB.addTagGroup("carpitype")
+#trialDB.addTag("carpitype", "acricarp")
+#trialDB.addTag("carpitype", "plaricarp")
+#trialDB.addDatum("Bryum")
+#trialDB.tagDatum("Bryum", "leaf angle", "acute leaf")
+#trialDB.tagDatum("Bryum", "carpitype", "acricarp")
+#trialDB.tagDatum("Bryum", "leaf angle", "obtuse leaf")
+
+#trialDB.addTagGroup("leaf margin", ("toothed leaf", "smooth leaf"))
+#trialDB.addDatum("notBryum", ("acute leaf", "plaricarp"))
+#trialDB.addDatum("a", ("obtuse leaf"))
+#trialDB.addDatum("b", ("obtuse leaf", "plaricarp", "smooth leaf"))
+#trialDB.addDatum("c", ("acute leaf"))
+#trialDB.addDatum("d", ("obtuse leaf", "acricarp", "toothed leaf"))
+#trialDB.addDatum("e", ("acricarp"))
+#trialDB.addDatum("f", ("plaricarp", "toothed leaf"))
+#trialDB.addDatum("g", ("obtuse leaf", "acricarp", "toothed leaf"))
+print(trialDB.getTagGroups())
+
+while True:
+	try:
+		print(eval(input("<<< ")))
+	except Exception as E:
+		print(E)
